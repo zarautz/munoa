262047f81765b834de898b086a2c8e021ae1759d
Eneko Illarramendi <eneko@illarra.com>
2013-05-06 19:56:49 +0200

    Initial commit

---
+++ assets/config.rb
@@ -0,0 +1,12 @@
+require "zurb-foundation"
+
+http_path       = "/"
+css_dir         = "css"
+sass_dir        = "scss"
+images_dir      = "images"
+fonts_dir       = "fonts"
+javascripts_dir = "js"
+project_type    = :stand_alone
+output_style    = :compressed
+line_comments   = false
+relative_assets = true

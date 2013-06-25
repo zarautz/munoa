b47fea4f6407776da0df018051f25fd8cfd8dadd
doup <aillarramendi@gmail.com>
2013-06-25 22:18:52 +0200

    basurilla fuera

--- app/scripts/init.js
+++ app/scripts/init.js
@@ -5,6 +5,8 @@ var Z = {};
 // Create the Angular app
 Z.app = angular.module('zarautz', ['ngMobile']);
 
+Z.app.value('host', false ? 'http://zarautz.illarra.com' : 'http://local.data.zarautz.org');
+
 Z.app.run(['$rootScope', 'babel', function ($rootScope, babel) {
     $rootScope.__ = function (key) {
         return babel.translate(key);

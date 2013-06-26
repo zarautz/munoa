19be9d41ca8724cead426884c762f0f2924c117f
doup <aillarramendi@gmail.com>
2013-06-26 13:36:41 +0200

    translate filtroa gehituta

--- app/scripts/init.js
+++ app/scripts/init.js
@@ -6,8 +6,8 @@ Z.app = angular.module('munoa.zarautz', ['ngResource', 'ngMobile']);
 
 Z.app.value('apiHost', true ? 'http://pagoeta.zarautz.org/app_dev.php/v1' : 'http://local.data.zarautz.org');
 
-Z.app.run(['$rootScope', 'babel', function ($rootScope, babel) {
-    $rootScope.__ = function (key) {
+Z.app.filter('translate', ['babel', function (babel) {
+    return function (key) {
         return babel.translate(key);
     };
 }]);

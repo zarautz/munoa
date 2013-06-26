75cb9fe470cec407c8a05a4c61a4d06359d255a4
doup <aillarramendi@gmail.com>
2013-06-26 22:50:08 +0200

    Cache store.js-kin

---
+++ app/scripts/service/cache.js
@@ -0,0 +1,38 @@
+'use strict';
+
+Z.app.factory('cache', ['$log', function(log) {
+    var store = window.store;
+
+    return {
+        set: function(key, value, ttl) {
+            ttl  = ttl || 0;
+            ttl *= 1000;
+
+            log.info('[CACHE] Saving "' + key + '", TTL: ' + (ttl / 1000) + 's');
+
+            store.set(key, { value: value, ttl: ttl, time: (new Date()).getTime() });
+        },
+        get: function(key) {
+            var data = store.get(key);
+
+            // Check if data is found
+            if (!data) {
+                log.info('[CACHE] "' + key + '" not found');
+
+                return null;
+            }
+
+            // Check if the entry has expired
+            if (data.ttl > 0 && (new Date()).getTime() - data.time > data.ttl) {
+                log.info('[CACHE] "' + key + '" expired');
+
+                return null;
+            }
+
+            // It's a valid entry
+            log.info('[CACHE] "' + key + '" found');
+
+            return data.value;
+        }
+    };
+}]);
--- app/scripts/service/event.js
+++ app/scripts/service/event.js
@@ -1,19 +1,19 @@
 'use strict';
 
-Z.app.factory('event', ['$resource', 'apiHost', '$cacheFactory', function($resource, apiHost, $cacheFactory) {
-    var cache    = $cacheFactory('event');
-    var resource = $resource(
+Z.app.factory('event', ['$resource', 'apiHost', 'cache', function($resource, apiHost, cache) {
+    var store = $resource(
         apiHost + '/events/:id',
         { id: '@id' }
     );
 
     return {
         findAll: function () {
-            var events = cache.get('all');
+            var events = cache.get('events.all');
 
             if (!events) {
-                events = resource.query();
-                cache.put('all', events);
+                events = store.query(function (data) {
+                    cache.set('events.all', data, 10);
+                });
             }
 
             return events;

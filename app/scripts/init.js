ed0cdb08edc7588580895191ea2c529a87738c0c
Eneko Illarramendi <eneko@illarra.com>
2013-06-26 18:55:14 +0200

    App ID updated

--- app/scripts/init.js
+++ app/scripts/init.js
@@ -2,7 +2,7 @@
 
 var Z = {};
 
-Z.app = angular.module('munoa.zarautz', ['ngResource', 'ngMobile']);
+Z.app = angular.module('munoa', ['ngResource', 'ngMobile']);
 
 Z.app.value('apiHost', true ? 'http://pagoeta.zarautz.org/app_dev.php/v1' : 'http://local.data.zarautz.org');
 

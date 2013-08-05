3a669d10396a4b8bb1741270afe07eb0aed18212
doup <aillarramendi@gmail.com>
2013-08-05 18:55:51 +0200

    Defektuz goiko zerbitzaritik hartu datuak

--- app/scripts/init.js
+++ app/scripts/init.js
@@ -4,7 +4,7 @@ var Z = {};
 
 Z.app = angular.module('munoa', ['ngResource', 'ngMobile', 'ngSanitize']);
 
-Z.app.value('apiHost', true ? 'http://pagoeta.zarautz.org/app_dev.php/v1' : 'http://local.data.zarautz.org');
+Z.app.value('apiHost', true ? 'http://pagoeta.illarra.com/v1' : 'http://pagoeta.zarautz.org/app_dev.php/v1');
 
 Z.app.filter('translate', ['babel', function (babel) {
     return function (key) {

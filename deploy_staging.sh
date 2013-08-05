027bfd729b69d6492811d4e1cffde692bc20f4f1
doup <aillarramendi@gmail.com>
2013-08-05 18:55:37 +0200

    Zerbitzarira deploya gehituta

---
+++ deploy_staging.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Deploying to sugaar..."
+
+# Build the App
+grunt build --force
+
+# Copy phonegap-desktop to dist
+cp ./phonegap-desktop/debugdata.json ./dist/debugdata.json
+cp ./phonegap-desktop/js/phonegap-desktop.js ./dist/phonegap.js
+
+# Clean remote folder
+ssh zarautz@37.153.97.168 "rm -rf ~/munoa/staging/current/web/*"
+
+# () keeps the original folder before "cd"
+(cd dist; scp -r . zarautz@37.153.97.168:/home/zarautz/munoa/staging/current/web)

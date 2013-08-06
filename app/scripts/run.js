c9ca433ed0df5185082fec9febbb52a219ca78f0
doup <aillarramendi@gmail.com>
2013-08-06 18:30:31 +0200

    Loading feedback prueba bat

---
+++ app/scripts/run.js
@@ -0,0 +1,22 @@
+Z.app.run(function ($http, $rootScope) {
+    // -----------
+    // NET POOLING
+    // -----------
+    $http.defaults.transformRequest.push(function (data) {
+        $rootScope.$broadcast('httpCallStarted');
+        return data;
+    });
+
+    $http.defaults.transformResponse.push(function (data) {
+        $rootScope.$broadcast('httpCallStopped');
+        return data;
+    });
+
+    $rootScope.$on('httpCallStarted', function () {
+        $rootScope.loading = true;
+    });
+
+    $rootScope.$on('httpCallStopped', function () {
+        $rootScope.loading = false;
+    });
+});

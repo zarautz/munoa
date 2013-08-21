534f7b46e4450330e0ad4f816d89c142e38e2548
Eneko Illarramendi <eneko@illarra.com>
2013-08-21 18:09:52 +0200

    Eguraldi tabak martxan

--- app/scripts/controller/forecast.js
+++ app/scripts/controller/forecast.js
@@ -1,12 +1,17 @@
 'use strict';
 
 Z.app.controller('ForecastController', ['forecast', function(forecast) {
-    this.forecast = forecast.findAll();
-    this.today    = this.forecast.data[0];
+    this.forecast  = forecast.findAll();
+    this.today     = this.forecast.data[0];
+    this.activeTab = 0;
 
     this.getMainForecastCode = function (forecast) {
         var selectionByLength = { 4: 2, 2: 1, 1: 0};
 
         return forecast[selectionByLength[forecast.length]]['code'];
+    }
+
+    this.activateTab = function (index) {
+        this.activeTab = index;
     }
 }]);

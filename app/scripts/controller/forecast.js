2d8698f44b5ce146a0147bf73b5364c7a4902505
Eneko Illarramendi <eneko@illarra.com>
2013-08-21 20:58:43 +0200

    Gero testeatu ahal izateko aldaketatxo batzuk

--- app/scripts/controller/forecast.js
+++ app/scripts/controller/forecast.js
@@ -1,27 +1,27 @@
 'use strict';
 
 Z.app.controller('ForecastController', ['forecast', 'weatherCode', function(forecast, weatherCode) {
+    var now = new Date();
+
     this.forecast     = forecast.findAll();
     this.today        = this.forecast.data[0];
+    this.hour         = now.getHours();
     this.activeTab    = 0;
     this.weatherCodes = weatherCode.findAll().data;
 
-    this.forecastIsCurrent = function (index) {
+    this.forecastIsToday = function (index) {
         return index === 0;
     };
 
     this.getCurrentHourIndex = function () {
-        var now = new Date();
-
-        return Math.floor(now.getHours() / (24 / this.today.weather.forecast.length));
+        return Math.floor(this.hour / (24 / this.today.weather.forecast.length));
     };
 
-    this.getMainWeatherCode = function (forecast, useCurrentHour) {
-        var now, key;
+    this.getForecastWeatherCode = function (forecast, isToday) {
+        var key;
 
-        if (useCurrentHour) {
-            now = new Date();
-            key = Math.floor(now.getHours() / (24 / forecast.length));
+        if (isToday) {
+            key = Math.floor(this.hour / (24 / forecast.length));
         } else {
             key = Math.floor(12 / (24 / forecast.length));
         }

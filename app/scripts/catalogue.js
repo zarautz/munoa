321fc2a88c66c256bde424cc8a0553d9661cb4b8
Eneko Illarramendi <eneko@illarra.com>
2013-08-22 13:56:50 +0200

    CSS aldaketak eta katalogoko entrada berriak (i18n)

--- app/scripts/catalogue.js
+++ app/scripts/catalogue.js
@@ -7,12 +7,44 @@ Z.catalogue = {
         'eu': 'Ekitaldiak',
         'fr': ''
     },
+    // Weather
     'forecast': {
         'en': 'Weather',
         'eu': 'Eguraldia',
         'es': 'El tiempo',
         'fr': ''
     },
+    'weather.uv.title': {
+        'en': 'Weather',
+        'eu': 'Kontuz gaur UV izpiekin',
+        'es': 'Cuidado hoy con los rayos UV',
+        'fr': ''
+    },
+    'weather.uv.text': {
+        'en': 'Wear sunglasses and use SPF 30+ sunscreen, cover the body with sun protective clothing and a wide-brim hat, and reduce time in the sun from three hours before to three hours after solar noon.',
+        'eu': 'Wear sunglasses and use SPF 30+ sunscreen, cover the body with sun protective clothing and a wide-brim hat, and reduce time in the sun from three hours before to three hours after solar noon.',
+        'es': 'El tiempo',
+        'fr': "Réduire l’exposition entre 11 h et 16 h. Appliquer un écran solaire de haute protection (indice de 30 à 50), porter un chapeau et des lunettes de soleil, et se placer à l’ombre."
+    },
+    'weather.uv.orange': {
+        'en': 'Wear sunglasses and use SPF 30+ sunscreen, cover the body with sun protective clothing and a wide-brim hat, and reduce time in the sun from three hours before to three hours after solar noon.',
+        'eu': '',
+        'es': '',
+        'fr': "Réduire l’exposition entre 11 h et 16 h. Appliquer un écran solaire de haute protection (indice de 30 à 50), porter un chapeau et des lunettes de soleil, et se placer à l’ombre."
+    },
+    'weather.uv.red': {
+        'en': 'Wear SPF 30+ sunscreen, a shirt, sunglasses, and a hat. Do not stay out in the sun for too long.',
+        'eu': '',
+        'es': '',
+        'fr': "Sans protection, la peau sera endommagée et peut brûler. L’exposition au soleil peut être dangereuse entre 11 h et 16 h ; la recherche de l’ombre est donc importante. Sont recommandables le port de vêtements longs, d’un chapeau et de lunettes de soleil, ainsi que l’application d'un écran solaire de très haute protection (indice + 50)."
+    },
+    'weather.uv.violet': {
+        'en': 'Take all precautions, including: wear sunglasses and use SPF 30+ sunscreen, cover the body with a long-sleeve shirt and trousers, wear a very broad hat, and avoid the sun from three hours before until three hours after solar noon.',
+        'eu': '',
+        'es': '',
+        'fr': "La peau non protégée sera endommagée et peut brûler en quelques minutes. Toute exposition au soleil est dangereuse, et en cas de sortie il faut se couvrir absolument (chapeau, lunettes de soleil, application d'un écran solaire de très haute protection d'indice + 50)."
+    },
+    // Health
     'health': {
         'en': '',
         'eu': 'Osasun zerbitzuak',

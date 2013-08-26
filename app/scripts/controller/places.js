738debb1d1e921de0f5a6a3b2ed21e2f36e1d143
doup <aillarramendi@gmail.com>
2013-08-26 21:44:26 +0200

    Use controller

--- app/scripts/controller/places.js
+++ app/scripts/controller/places.js
@@ -1,12 +1,26 @@
-/*
-'health',
-'wifi',
-'atm',
-'stores',
-'parking',
-'sports',
-'transport',
-'recycling',
-'gastronomy',
-'lodging'
-*/
+'use strict';
+
+Z.app.controller('PlacesController', ['$scope', 'placesMapper', 'settings', function($scope, placesMapper, settings) {
+    /*
+        'health',
+        'wifi',
+        'atm',
+        'stores',
+        'parking',
+        'sports',
+        'transport',
+        'recycling',
+        'gastronomy',
+        'lodging'
+
+        http://www.geodatasource.com/developers/javascript
+    */
+    $scope.refresh = function () {
+        var places = placesMapper.get({'language': settings.get('language'), 'types': 'health'});
+
+        this.places = places.promise;
+        this.status = places.status;
+    };
+
+    $scope.refresh();
+}]);

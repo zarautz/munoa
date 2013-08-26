d612cb48fd7edd166fd8eda8df0c1a21dd4b825a
doup <aillarramendi@gmail.com>
2013-08-26 17:52:53 +0200

    Wizard πeta

--- app/scripts/controller/wizard.js
+++ app/scripts/controller/wizard.js
@@ -1,7 +1,31 @@
 'use strict';
 
-Z.app.controller('WizardController', ['$scope', 'settings', function($scope, settings) {
+Z.app.controller('WizardController', ['$scope', 'babel', 'menu', 'settings', function($scope, babel, menu, settings) {
+    this.page     = 0;
+    this.settings = settings;
+
+    this.back = function () {
+        this.page--;
+    }
+
     this.isActive = function() {
-       return !settings.isCached()
-   };
+        if (this.page > 1) {
+            this.page = 0;
+        }
+
+        return !settings.isCached();
+    };
+
+    this.updateSetting = function(id, option) {
+        if (id === 'profile') {
+            menu.setProfile(option);
+        }
+
+        if (id === 'language') {
+            babel.setLanguage(option);
+        }
+
+        settings.set(id, option);
+        this.page++;
+    };
 }]);

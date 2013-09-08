1bfb374bf9ce48b92afe075d2baa7b9f542b6873
doup <aillarramendi@gmail.com>
2013-09-09 00:06:25 +0200

    Splasha kenduta

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,9 +9,11 @@ Z.app.value('googleApiKey', 'AIzaSyBopLXCM1kLoklpwOOvyA_QurVbj1H02C0');
 
 // We force early initialization by injecting the services
 Z.app.run(['$timeout', 'phonegap', function ($timeout, phonegap) {
+    /*
     phonegap.onDeviceReady().then(function () {
         $timeout(function () {
             navigator.splashscreen.hide();
-        }, 1000);
+        }, 250);
     });
+    */
 }]);

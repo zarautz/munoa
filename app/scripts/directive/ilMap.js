807bbbd7c3b52a6fc31d5997c3675ba873c4cf99
Eneko Illarramendi <eneko@illarra.com>
2013-09-10 20:36:27 +0200

    Mapa aldatuta, geolokalizazioa ez dagoenean ez erakusteko

--- app/scripts/directive/ilMap.js
+++ app/scripts/directive/ilMap.js
@@ -4,53 +4,53 @@ Z.app.directive('ilMap', ['geolocation', function (geolocation) {
     return function (scope, elem, attrs) {
         var latitude,
             longitude,
-            map, bounds, latLng, marker1, marker2,
-            buildMap,
-            userLatitude = geolocation.getCurrentPosition().location.latitude,
-            userLongitude = geolocation.getCurrentPosition().location.longitude,
-            userLatLng = new google.maps.LatLng(userLatitude, userLongitude),
-            fakeLat, fakeLng;
+            map, bounds, latLng, marker, userMarker,
+            userLocation = geolocation.getCurrentPosition().location,
+            buildMap;
 
         buildMap = function() {
             latitude = parseFloat(attrs.latitude, 10) || 43;
             longitude = parseFloat(attrs.longitude, 10) || 2;
             latLng = new google.maps.LatLng(latitude, longitude);
-            bounds = new google.maps.LatLngBounds();
 
             map = new google.maps.Map(elem[0], {
-                zoom: 10,
-                center: new google.maps.LatLng(0, 0),
+                zoom: 16,
+                center: latLng,
                 mapTypeControl: false,
-                maxZoom: 18
+                zoomControl: false,
+                streetViewControlOptions: {
+                    position: google.maps.ControlPosition.RIGHT_BOTTOM
+                },
+                maxZoom: 19
             });
-            marker1 = new google.maps.Marker({
+
+            marker = new google.maps.Marker({
                 position: latLng,
                 map: map
             });
-            marker2 = new google.maps.Marker({
-                position: userLatLng,
-                map: map,
-                icon: {
-                    path: google.maps.SymbolPath.CIRCLE,
-                    fillOpacity: 0.5,
-                    fillColor: '0099cc',
-                    strokeOpacity: 1.0,
-                    strokeColor: '0099cc',
-                    strokeWeight: 3.0,
-                    scale: 10 //pixels
-                }
-            });
 
-            // Calculate fake point for bounding
-            fakeLat = userLatitude - (latitude - userLatitude);
-            fakeLng = userLongitude - (longitude - userLongitude);
+            // Set user marker if we have location
+            if (userLocation !== false) {
+                userMarker = new google.maps.Marker({
+                    position: new google.maps.LatLng(userLocation.latitude, userLocation.longitude),
+                    map: map,
+                    icon: {
+                        path: google.maps.SymbolPath.CIRCLE,
+                        fillOpacity: 0.5,
+                        fillColor: '0099cc',
+                        strokeOpacity: 1.0,
+                        strokeColor: '0099cc',
+                        strokeWeight: 3.0,
+                        scale: 10 //pixels
+                    }
+                });
 
-            bounds.extend(new google.maps.LatLng(fakeLat, fakeLng));
-            bounds.extend(marker1.getPosition());
-            map.fitBounds(bounds);
-
-            // Set center
-            map.setCenter(userLatLng);
+                // Set bounds
+                bounds = new google.maps.LatLngBounds();
+                bounds.extend(marker.getPosition());
+                bounds.extend(userMarker.getPosition());
+                map.fitBounds(bounds);
+            }
         };
 
         attrs.$observe('latitude', buildMap);

ec662c4fee0e685a35f58e2e0759369cb61c8b4a
Eneko Illarramendi <eneko@illarra.com>
2013-12-04 15:45:42 +0100

    Blogean irudi bati garrantzia ematen bazaio, hori erabili gure zerrendetan ere

--- app/scripts/model/zuZarautz.js
+++ app/scripts/model/zuZarautz.js
@@ -20,10 +20,15 @@ Z.Model.ZuZarautz.prototype.getContent = function (size) {
 };
 
 Z.Model.ZuZarautz.prototype.getThumbnailImage = function () {
-    var images = this.contentImages,
+    var files  = this.files,
+        images = this.contentImages,
         keys   = Object.keys(images);
 
-    if (keys.length > 0) { // TODO: check if user is connected
+    // If an image is featured in ZuZarautz, goes to "files"
+    // Use it by default
+    if (files.length > 0 && files[0].type === 'image/jpg') {
+        return files[0].image.source.square;
+    } else if (keys.length > 0) { // TODO: check if user is connected
         return images[keys[0]].source.square;
     } else {
         return 'images/dot.png';
